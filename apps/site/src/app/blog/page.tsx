e94c6a97632b17728e18265821a49e0650dc73ec
parallexstudio <Theparallexstudio@gmail.com>
2026-08-21 18:09:29 +0530

    Blog: 2-column grid on mobile

--- apps/site/src/app/blog/page.tsx
+++ apps/site/src/app/blog/page.tsx
@@ -255,7 +255,7 @@ export default async function BlogPage({
               <p>No articles found. Try a different filter or search.</p>
             </div>
           ) : (
-            <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="mt-8 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4">
               {posts.map((post) => (
                 <Link
                   key={post.id}
